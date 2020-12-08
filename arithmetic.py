f619a98eeb1097d7aafb4cfcad942f51c645692c
ictwise <57628753+ictwise@users.noreply.github.com>
2020-12-08 11:48:11 +0000

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,57 @@
+# Exercise 1: Carry out the following calculations, and use print() to view your answers.
+
+# For example: for 3 x 5 the syntax (way of writing) would be:
+
+print(3 * 5)
+
+print(3 -5)
+
++
+-
+/
+*
+** 
+%
+
+print(5 % 2)
+
+
+# a) 20 subtract 5
+
+
+
+# b) 7 added to 6
+
+# c) 13 multiplied by 5 
+
+# d) 36 divided by 6 
+
+# e) 13 to the power of 3 (Exponentiation raises the number to the left to the power of the number to the right)
+
+# f) 51 modulo 7 (This operator returns the remainder from division of the two numbers)
+
+# g) Experiment with using parenthesis (curved brackets) to change the order of operations:
+
+# for example try 5*8-5 and compare the answer to 5*(8-5)
+
+# Note, you should see with your answer to g) that any calculation in brackets happens before any other calculation
+
+print(5*8-5)
+
+print(5*(8-5))
+
+#### Optional extras #####
+
+# h) Calculate 57.45/9.765;
+
+
+# i) Use the round() function to round your answer to h)
+
+
+
+# i) See if you can adjust the round function to round to 1 decimal place (you might have to google this)
+
+
+
+
+
